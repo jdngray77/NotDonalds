bd493a4c5536bef8cc9939ccf2c4cdf139cfc6a3
Shinkson47 <50697488+Shinkson47@users.noreply.github.com>
2020-08-03 23:15:15 +0100

    Commented abstract price extentions

--- src/sale/price/LongPrice.java
+++ src/sale/price/LongPrice.java
@@ -1,15 +1,39 @@
 package sale.price;
 
+/**
+ * Price with both partitions utilising Long Integer.
+ *
+ * Uses more memory and processing power, but can store considerably larger prices.
+ * Both halves of a price are an instance of Long,
+ * limited by <code>Long.MAX_VALUE</code> to <code>Long.MIN_VALUE</code>
+ *
+ * @inheritDoc
+ */
 public final class LongPrice extends Price<Long> {
 
+    /**
+     * Create a LongPrice with default value
+     *
+     * @inheritDoc
+     */
     public LongPrice(Long _decimal, Long _fractional) {
         super(_decimal, _fractional);
     }
 
+    /**
+     * Create a LongPrice with default value and currency symbol.
+     *
+     * @inheritDoc
+     */
     public LongPrice(Long _decimal, Long _fractional, char _symbol) {
         super(_decimal, _fractional, _symbol);
     }
 
+    /**
+     * Create a LongPrice with default value, symbol, and specified rtl.
+     *
+     * @inheritDoc
+     */
     public LongPrice(Long _decimal, Long _fractional, char _symbol, boolean _rtl) {
         super(_decimal, _fractional, _symbol, _rtl);
     }
--- src/sale/price/ShortPrice.java
+++ src/sale/price/ShortPrice.java
@@ -1,18 +1,40 @@
 package sale.price;
 
+
 /**
+ * Price with both partitions utilising Short Integer.
  *
+ * Uses less memory and processing power, but can store reasonably large prices.
+ * Both halves of a price are an instance of Short,
+ * limited by <code>Short.MAX_VALUE</code> to <code>Short.MIN_VALUE</code>
+ *
+ * @inheritDoc
  */
 public final class ShortPrice extends Price<Short> {
 
+    /**
+     * Create a ShortPrice with default value, symbol, and specified rtl.
+     *
+     * @inheritDoc
+     */
     public ShortPrice(Short _decimal, Short _fractional) {
         super(_decimal, _fractional);
     }
 
+    /**
+     * Create a ShortPrice with default value, symbol, and specified rtl.
+     *
+     * @inheritDoc
+     */
     public ShortPrice(Short _decimal, Short _fractional, char _symbol) {
         super(_decimal, _fractional, _symbol);
     }
 
+    /**
+     * Create a ShortPrice with default value, symbol, and specified rtl.
+     *
+     * @inheritDoc
+     */
     public ShortPrice(Short _decimal, Short _fractional, char _symbol, boolean _rtl) {
         super(_decimal, _fractional, _symbol, _rtl);
     }
