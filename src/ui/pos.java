7798ae5b77f5e225fb9c6d5ba757823881d907f2
Shinkson47 <business@shinkson47.in>
2020-08-06 19:47:32 +0100

    Simple Packet testing form

--- src/ui/pos.java
+++ src/ui/pos.java
@@ -1,11 +1,15 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class pos extends Application {
