fb569af8bbf74d40462f12809a1f3c92971f7800
Shinkson47 <business@shinkson47.in>
2020-08-18 13:52:47 +0100

    Ditched vaadin
    Tested pos module with vaadin, but ultimately decided to stick with jfx.
    - seperated front end and library into modules

--- pos/src/ui/jfx/pos.java
+++ pos/src/ui/jfx/pos.java
@@ -1,15 +1,12 @@
-package ui;
+package ui.jfx;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
+import javax.swing.*;
 import java.io.IOException;
 
 public class pos extends Application {
@@ -21,9 +18,12 @@ public class pos extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent rootfxml = FXMLLoader.load(getClass().getResource("pos.fxml"));
-
         primaryStage.setTitle("NotDonalds");
         primaryStage.setScene(new Scene(rootfxml, 1920, 1080));
         primaryStage.show();
+    }
+
+    public void test(){
+        JOptionPane.showMessageDialog(null, "The method call works!");
     }
 }
